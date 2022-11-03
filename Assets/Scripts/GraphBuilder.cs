7e09bfff1bd6b331eb37b3e6b87e1978e3a83b44
Lenur <93513097+LenurAm@users.noreply.github.com>
2022-11-03 22:12:56 +1300

    gitignore_settings

---
+++ Assets/Scripts/GraphBuilder.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Builds the graph
+/// </summary>
+public class GraphBuilder : MonoBehaviour
+{
+    static Graph<Waypoint> graph=new Graph<Waypoint>();
+
+    #region Constructor
+
+    // Uncomment the code below after copying this class into the console
+    // app for the automated grader. DON'T uncomment it now; it won't
+    // compile in a Unity project
+
+    /// <summary>
+    /// Constructor
+    /// 
+    /// Note: The GraphBuilder class in the Unity solution doesn't 
+    /// use a constructor; this constructor is to support automated grading
+    /// </summary>
+    /// <param name="gameObject">the game object the script is attached to</param>
+    //public GraphBuilder(GameObject gameObject) :
+    //    base(gameObject)
+    //{
+    //}
+
+    #endregion
+
+    /// <summary>
+    /// Awake is called before Start
+    ///
+    /// Note: Leave this method public to support automated grading
+    /// </summary>
+    public void Awake()
+    {
+        // add nodes (all waypoints, including start and end) to graph
+
+        List<GameObject> gameObjects = new List<GameObject>();
+        gameObjects.AddRange(GameObject.FindGameObjectsWithTag("Waypoint"));
+        ////.AddRange(GameObject.FindWithTag("Waypoint"));
+        //for (int i = 0; i < 30; i++)
+        //{
+
+        //    gameObjects[i].Add(GameObject.FindGameObjectWithTag("Waypoint"));
+        //}
+        Waypoint start = GameObject.FindGameObjectWithTag("Start").GetComponent<Waypoint>();
+        graph.AddNode(start);
+        Waypoint end = GameObject.FindGameObjectWithTag("End").GetComponent<Waypoint>();
+        graph.AddNode(end);
+        foreach (GameObject gameObject in GameObject.FindGameObjectsWithTag("Waypoint"))
+        {
+            Waypoint waypoint = gameObject.GetComponent<Waypoint>();
+            graph.AddNode(waypoint);    
+        }
+        
+
+        
+        // add neighbors for each node in graph
+        foreach (GraphNode<Waypoint> firstNode in graph.Nodes)
+        {
+            foreach (GraphNode<Waypoint> secondNode in graph.Nodes)
+            {
+                if (firstNode != secondNode)
+                {
+                    Vector2 positionDelta = firstNode.Value.Position
+                        - secondNode.Value.Position;
+                    if (Mathf.Abs(positionDelta.x) < 3.5 &&
+                        Mathf.Abs(positionDelta.y) < 3)
+                    {
+                        firstNode.AddNeighbor(secondNode, positionDelta.magnitude);
+                    }
+
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Gets and sets the graph
+    /// 
+    /// CAUTION: Set should only be used by the autograder
+    /// </summary>
+    /// <value>graph</value>
+    public static Graph<Waypoint> Graph
+    {
+        get { return graph; }
+        set { graph = value; }
+    }
+}
---
+++ Assets/Scripts/SortedLinkedList.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A sorted linked list
+/// </summary>
+public class SortedLinkedList<T> : LinkedList<T> where T:IComparable
+{
+    #region Constructor
+
+    public SortedLinkedList() : base()
+    {
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Adds the given item to the list
+    /// </summary>
+    /// <param name="item">item to add to list</param>
+    public void Add(T item)
+    {
+        //add your code here
+        // add your code here
+        if (First == null)
+        {
+            AddFirst(item);
+        }
+        //else if (Contains(item))
+        //{
+
+        //}
+        else if (First.Value.CompareTo(item) > 0)
+        {
+            AddFirst(item);
+        }
+        else
+        {
+            Reposition(item);
+        }
+    }
+
+    /// <summary>
+    /// Repositions the given item in the list by moving it
+    /// forward in the list until it's in the correct
+    /// position. This is necessary to keep the list sorted
+    /// when the value of the item changes
+    /// </summary>
+    public void Reposition(T item)
+    {
+        // add your code here
+        LinkedListNode<T> list = Find(item);
+        if (Contains(item))
+        {
+            if (list == null)
+            {
+                throw new Exception("Null exception");
+            }
+            else if (First.Value.CompareTo(list.Value) >= 0)
+            {
+                AddFirst(item);
+            }
+            else
+            {
+                LinkedListNode<T> previousNode = null;
+                LinkedListNode<T> currentNode = First;
+                while (currentNode != null && currentNode.Value.CompareTo(item) <= 0)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                }
+                if (currentNode == null)
+                {
+                    AddLast(item);
+                }
+                else
+                {
+                    AddBefore(currentNode, item);
+                }
+            }
+        }
+    }
+
+    #endregion
+}
